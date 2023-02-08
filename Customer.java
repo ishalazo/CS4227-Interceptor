5c4efb9845b79a1354c0a4ca365e796105902332
Isha Lazo <lakeisha.lazo@gmail.com>
2023-02-08 12:41:16 +0000

    Application skeleton code

---
+++ Customer.java
@@ -0,0 +1,14 @@
+public class Customer {
+    
+    public String statement(){
+        return "";
+    }
+
+    public double getTotalCharge(){
+        return 0.0;
+    }
+
+    public int getFrequentRenterPoints(){
+        return 0;
+    }
+}
---
+++ Rental.java
@@ -0,0 +1,12 @@
+public class Rental {
+    private int daysRented;
+
+    public double getCharge(){
+        return 0.0;
+    }
+
+    public int getFrequentRenterPoints(){
+        return 0;
+    }
+
+}
